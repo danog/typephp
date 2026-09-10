5b359d9c7bb118a978b768eb6db7f53b1f5f4cd4
Daniil Gentili <daniil@daniil.it>
2026-09-10 21:04:08 +0200

    Dynamic property reads inside a class carry the class scope (private/protected of other instances)

--- src/Parser/PropertyAccessTrait.php
+++ src/Parser/PropertyAccessTrait.php
@@ -113,6 +113,11 @@ trait PropertyAccessTrait
             return 'typephp_read_property_scoped('
                 . $object . ', ' . $property . ', php::FakeScopeGuard::current(), php::AttrMode::Get)';
         }
+        if ($this->class && !($this->classDef?->trait)) {
+            return 'typephp_read_property_scoped('
+                . $object . ', ' . $property . ', ' . $this->getLocalClassEntryPtr($this->getFullClassName())
+                . ', php::AttrMode::Get)';
+        }
         if ($cache !== null) {
             return 'typephp_read_property_cached('
                 . $object . ', ' . $property . ', php::AttrMode::Get, ' . $cache . ')';
@@ -1259,6 +1264,13 @@ trait PropertyAccessTrait
         } elseif ($this->usesTraitPropertyScope($objectVar)) {
             $getProperty = 'typephp_read_property_scoped('
                 . $objectVar . ', ' . $id . ', php::FakeScopeGuard::current(), ' . $attrMode . ')';
+        } elseif ($this->isIdExpr($property) && !$this->isNativePropertyAccess($expr) && $this->class) {
+            // inside a class the read carries the class scope, so private and
+            // protected properties of the receiver (another instance of this
+            // class or a parent) are visible as in PHP
+            $getProperty = 'typephp_read_property_scoped('
+                . $objectVar . ', ' . $id . ', ' . $this->getLocalClassEntryPtr($this->getFullClassName()) . ', '
+                . $attrMode . ')';
         } elseif ($this->isIdExpr($property) && !$this->isNativePropertyAccess($expr)) {
             $getProperty = 'typephp_read_property_cached('
                 . $objectVar . ', ' . $id . ', ' . $attrMode . ', '
